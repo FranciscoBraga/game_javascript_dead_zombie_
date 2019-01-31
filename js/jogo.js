4f0c2d5950528a74594e93e911b808e631efc222
Francisco Oliveira Braga <associacaogames@gmail.com>
2019-01-30 23:58:36 -0200

    imagem com posição aletoria

--- js/jogo.js
+++ js/jogo.js
@@ -9,3 +9,23 @@ function ajustaTamanhopalcoJogo() {
 }
 
 ajustaTamanhopalcoJogo();
+
+function posicaoRandomica() {
+	
+var posicaoX = Math.floor(Math.random() * altura)-90;
+var posicaoY = Math.floor(Math.random() * largura)-90;
+
+//para evitar que a posição - retire a imagem da janela visivel 
+posicaoY = posicaoY < 0 ? 0 : posicaoY;
+posicaoX = posicaoX < 0 ? 0 : posicaoX;
+
+//criando um elemento html
+var zombie = document.createElement('img');
+zombie.src = 'img/zombie.png';
+zombie.style.left=posicaoX+'px';
+zombie.style.top=posicaoY + 'px';
+zombie.style.position='absolute';
+//incluindo os elementos no body da página
+document.body.appendChild(zombie);
+
+}
